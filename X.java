b591d91ad55f6e93c4e85442a292c4c5121f5ad4
chidoript <chidoript@gmail.com>
2024-11-22 22:05:42 -0500

    Update X.java

--- X.java
+++ X.java
@@ -2,6 +2,6 @@ public class X {
     
     public static void main(String[] args) {
         
-        System.out.println("Esto es un mensaje");
+        System.out.println("Hello World");
     }
 }
